072c0d05e58d58a805c41ae5957f9ebc00b8202e
Carter Kozak <ckozak@redhat.com>
2013-04-25 21:15:45 -0400

    added thermometer code

--- thermometer.py
+++ thermometer.py
@@ -18,6 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import threading
 import random
 import sys
+import os
+import glob
+import time
 from observable import Observable
 
 class ThermometerSingleton(Observable):
@@ -35,8 +38,33 @@ class ThermometerSingleton(Observable):
         super(ThermometerSingleton,self).__init__()
         self.update_interval = update_interval
 
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        self.device_file = device_folder + '/w1_slave'
+
+    def read_temp_raw(self):
+        f = open(self.device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def read_temp(self):
+        lines = read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_f
+
     def get_temp(self):
-        return random.randint(50,100)
+        #return random.randint(50,100)
+        return self.read_temp()
 
     def notify_observers(self):
         temp = self.get_temp()
