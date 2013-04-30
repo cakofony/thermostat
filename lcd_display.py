ade963d5617e98e2ae7672bde3ed272ba157a0ce
ckozak <ckozak@redhat.com>
2013-04-30 19:26:30 -0400

    updated lcd code

--- lcd_display.py
+++ lcd_display.py
@@ -1,7 +1,21 @@
-from adafruit_libs import Adafruit_CharLCDPlate
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 class LCDPlate():
 
     def __init__(self):
         self.lcd = Adafruit_CharLCDPlate()
-        
+        self.lcd.begin(16, 2)
+        self.lcd.clear()
+        self.lcd.backlight(lcd.ON)
+
+    def update_active(self, result):
+        if result = 'cool':
+            self.lcd.backlight(self.lcd.BLUE)
+        elif result = 'heat':
+            self.lcd.backlight(self.lcd.RED)
+        elif result = 'off':
+            self.lcd.backlifht(self.lcd.ON)
+
+    def update_temperature(self, temp):
+        self.lcd.clear()
+        self.lcd.message("Temp: %.1fF" % temp)
