3d86bf4e426a9f3a737d916c97e0ed6abf7654ce
Carter Kozak <ckozak@redhat.com>
2013-05-01 21:17:10 -0400

    changed lcd color again

--- lcd_display.py
+++ lcd_display.py
@@ -14,7 +14,7 @@ class LCDPlate():
         elif result == 'heat':
             self.lcd.backlight(self.lcd.RED)
         elif result == 'off':
-            self.lcd.backlight(self.lcd.OFF)
+            self.lcd.backlight(self.lcd.WHITE)
 
     def update_temperature(self, temp):
         self.lcd.clear()
