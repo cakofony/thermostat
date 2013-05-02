9b472f141e37324d30ac9d67b051c5a32b1d7021
Carter Kozak <ckozak@redhat.com>
2013-05-01 21:31:06 -0400

    purple

--- lcd_display.py
+++ lcd_display.py
@@ -14,7 +14,7 @@ class LCDPlate():
         elif result == 'heat':
             self.lcd.backlight(self.lcd.RED)
         elif result == 'off':
-            self.lcd.backlight(self.lcd.GREEN)
+            self.lcd.backlight(self.lcd.PURPLE)
 
     def update_temperature(self, temp):
         self.lcd.clear()
