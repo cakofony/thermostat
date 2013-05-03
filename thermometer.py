113a93f2eceac9668e192bfda116a2eeba6e0e65
ckozak <ckozak@redhat.com>
2013-05-03 17:03:24 -0400

    refactor therm

--- thermometer.py
+++ thermometer.py
@@ -41,14 +41,16 @@ class ThermometerSingleton(Observable):
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
         self.device_file = device_folder + '/w1_slave'
-
+    
+    # code adapted from adafruit raspberry pi libraries
     def read_temp_raw(self):
         f = open(self.device_file, 'r')
         lines = f.readlines()
         f.close()
         return lines
 
-    def read_temp(self):
+    # code adapted from adafruit raspberry pi libraries
+    def get_temp(self):
         lines = self.read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             print 'Error reading temp'
@@ -60,10 +62,6 @@ class ThermometerSingleton(Observable):
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
             return temp_f
-
-    def get_temp(self):
-        #return random.randint(50,100)
-        return self.read_temp()
 
     def notify_observers(self):
         temp = self.get_temp()
